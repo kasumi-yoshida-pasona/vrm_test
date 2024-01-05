b1dc93e3c82bfc7b4671addfabae7e4b2b43f11d
kasumi-yoshida-pasona <110513233+kasumi-yoshida-pasona@users.noreply.github.com>
2024-01-05 15:09:13 +0900

    ５０回分追加

--- Assets/Scripts/VRMTest.cs
+++ Assets/Scripts/VRMTest.cs
@@ -5,8 +5,8 @@ using System.IO;
 using VRMShaders;
 using UnityEngine.Networking;
 using System;
-using System.Collections;
 using System.Threading.Tasks;
+using UnityEngine.UI;
 
 namespace VRMTest
 {
@@ -15,64 +15,158 @@ public class VRMTest : MonoBehaviour
         [SerializeField]
         private RuntimeAnimatorController m_animCtrl;
         private string filePath;
-        private GameObject vrmObj;
-        private int count = 0;
+        [SerializeField] private Button OneTimeButton;
+        [SerializeField] private Button FiftyTimesButton;
+        [SerializeField] private Button FiveHundredsTimesButton;
 
         void Start()
         {
             filePath = Path.Combine(Application.streamingAssetsPath, "1903884660012638236.vrm");
-            StartCoroutine(AssetLoader());
+            OneTimeButton.onClick.AddListener(OnClickOneTime);
+            FiftyTimesButton.onClick.AddListener(OnClickFiftyTimes);
+            FiveHundredsTimesButton.onClick.AddListener(OnClick500Times);
+        }
+        void disableBtns()
+        {
+            OneTimeButton.interactable = false;
+            FiftyTimesButton.interactable = false;
+            FiveHundredsTimesButton.interactable = false;
         }
 
-        private void fiftyTimesVRMRuntimeLoad() {
-            if (count < 50)
-            {
-                StartCoroutine(AssetLoader());
-                count++;
-            }
+        void ableBtns()
+        {
+            OneTimeButton.interactable = true;
+            FiftyTimesButton.interactable = true;
+            FiveHundredsTimesButton.interactable = true;
         }
 
-        IEnumerator AssetLoader()
+        async void OnClickOneTime()
+        {
+            disableBtns();
+            await AssetLoader(1);
+            ableBtns();
+        }
+
+        async void OnClickFiftyTimes()
+        {
+            disableBtns();
+            await AssetLoader(50);
+            ableBtns();
+        }
+        async void OnClick500Times()
+        {
+            disableBtns();
+            await AssetLoaderWithoutDelete(50);
+            ableBtns();
+        }
+
+        // 指定回数分ランタイムロードしてGameObjectのVRMを返すX10
+        async Task AssetLoaderWithoutDelete(int loopNum)
         {
             bool isURI = Uri.IsWellFormedUriString(filePath, UriKind.Absolute);
             byte[] data = { };
 
-            if (isURI)
+            for (int n = 0; n < 10; n++)
             {
-                var request = UnityWebRequest.Get(filePath);
-                yield return request.SendWebRequest();
-
-                if (request.result == UnityWebRequest.Result.Success)
+                GameObject[] VRMList = new GameObject[loopNum];
+                for (int i = 0; i < loopNum; i++)
                 {
-                    data = request.downloadHandler.data;
-                    runtimeLoadVRMAsync(data, filePath);
+                    if (isURI)
+                    {
+                        var request = UnityWebRequest.Get(filePath);
+                        var requestAwaiter = request.SendWebRequest();
+
+                        while (!requestAwaiter.isDone)
+                        {
+                            await Task.Delay(10);
+                        }
+
+                        if (request.result == UnityWebRequest.Result.Success)
+                        {
+                            data = request.downloadHandler.data;
+                            GameObject vrmObj = await runtimeLoadVRMAsync(data, filePath);
+                            VRMList[i] = vrmObj;
+                        }
+                        else
+                        {
+                            Debug.Log(request.error);
+                        }
+                    }
+                    else
+                    {
+                        try
+                        {
+                            data = await File.ReadAllBytesAsync(filePath);
+                            GameObject vrmObj = await runtimeLoadVRMAsync(data, filePath);
+                            VRMList[i] = vrmObj;
+                        }
+                        catch(Exception ex)
+                        {
+                            Debug.Log(ex.Message);
+                        }
+                    }
+                }
+                for (int i = 0; i < loopNum; i++)
+                {
+                    deleteVRMAsync(VRMList[i]);
+                }
+            }
+
+        }
+
+
+        // 指定回数分ランタイムロードして破棄する
+        async Task AssetLoader(int loopNum)
+        {
+            bool isURI = Uri.IsWellFormedUriString(filePath, UriKind.Absolute);
+            byte[] data = { };
+
+            for (int i = 0; i < loopNum; i++)
+            {
+                if (isURI)
+                {
+                    var request = UnityWebRequest.Get(filePath);
+                    var requestAwaiter = request.SendWebRequest();
+
+                    while (!requestAwaiter.isDone)
+                    {
+                        await Task.Delay(10);
+                    }
+
+                    if (request.result == UnityWebRequest.Result.Success)
+                    {
+                        data = request.downloadHandler.data;
+                        GameObject vrmObj = await runtimeLoadVRMAsync(data, filePath);
+                        deleteVRMAsync(vrmObj);
+                    }
+                    else
+                    {
+                        Debug.Log(request.error);
+                    }
                 }
                 else
                 {
-                    Debug.Log(request.error);
-                    yield break;
-                }
-            }
-            else
-            {
-                try
-                {
-                    data = File.ReadAllBytes(filePath);
-                    runtimeLoadVRMAsync(data, filePath);
-                }
-                catch(Exception ex)
-                {
-                    Debug.Log(ex.Message);
-                    yield break;
+                    try
+                    {
+                        data = await File.ReadAllBytesAsync(filePath);
+                        GameObject vrmObj = await runtimeLoadVRMAsync(data, filePath);
+                        deleteVRMAsync(vrmObj);
+                    }
+                    catch(Exception ex)
+                    {
+                        Debug.Log(ex.Message);
+                    }
                 }
             }
         }
 
-        async void runtimeLoadVRMAsync(byte[] data, string path)
+        async Task<GameObject> runtimeLoadVRMAsync(byte[] data, string filePath)
         {
-            RuntimeGltfInstance instance = await VrmUtility.LoadBytesAsync(path, data, awaitCaller: (IAwaitCaller)new ImmediateCaller());
+            RuntimeGltfInstance instance = await VrmUtility.LoadBytesAsync(filePath, data, new RuntimeOnlyAwaitCaller());
+            // ↓はWebGLで並列処理を避けるためにImmediate
+            // RuntimeGltfInstance instance = await VrmUtility.LoadBytesAsync(filePath, data, awaitCaller: (IAwaitCaller)new ImmediateCaller());
             instance.ShowMeshes();
-            vrmObj = instance.gameObject;
+            GameObject vrmObj = instance.gameObject;
             if (vrmObj != null)
             {
                 var animCtrl = vrmObj.GetComponent<Animator>();
@@ -80,16 +174,17 @@ public class VRMTest : MonoBehaviour
                 {
                     animCtrl.runtimeAnimatorController = m_animCtrl;
                 }
-                await Task.Delay(1000);
-                DestroyModel();
+                await Task.Delay(100);
             }
+            return vrmObj;
         }
 
-        void DestroyModel()
+        void deleteVRMAsync(GameObject vrmObj)
         {
-            Destroy(vrmObj);
-            vrmObj = null;
-            Debug.Log("DestroyModel");
+            
+                // モデルの削除
+                Destroy(vrmObj);
+                vrmObj = null;
         }
 
     }
